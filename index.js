a9aa31ff07fab76152b31fb9bdd927b4bcb69a16
DeliaGlez <deliavargas.glez@gmail.com>
2024-09-13 15:30:35 -0700

    prueba mapa

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-canvas.width = 1024
-canvas.height = 576
+const map = new Image();
+map.src = './img/map.png'; 
 
-console.log("hola mundo");
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(map, -450, -400);
+    requestAnimationFrame(update);
+}
+
+
+map.onload = () => {
+    update();
+};
